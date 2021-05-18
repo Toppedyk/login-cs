39ed2716bec92ae87aea1f9f41e7b7523a2cdbeb
Toppedyk <Tayloroppedyk@yahoo.com>
2021-05-18 09:54:11 -0600

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace login.Models
+{
+  public class User
+  {
+    public string Name { get; set; }
+    private string Password { get; set; }
+
+    public bool ValidatePassword(string input)
+    {
+        return input == Password;
+    }
+    public User(string name, string password)
+    {
+      Name = name;
+      Password = password;
+
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using login.Models;
+
+namespace login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, User> users = new Dictionary<string, User>();
+            User taylor = new User("Taylor", "Hercules43");
+            User logan = new User("Logan", "LoganTest");
+            users.Add(taylor.Name, taylor);
+            users.Add(logan.Name, logan);
+
+
+            Console.Clear();
+            System.Console.WriteLine("Welcome, Please login to continue");
+            System.Console.Write("Username: ");
+            string username = Console.ReadLine();
+            System.Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            if(users.ContainsKey(username) && users[username].ValidatePassword(password))
+            {
+                System.Console.WriteLine("Successfully logged in");
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid Username or Password!");
+            }
+        }
+    }
+}
